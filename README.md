8451ae8733f04d95ae47c678e0fabc92a43dffea
Jordan Mele <jordan.mele@outlook.com.au>
2021-06-26 13:03:30 +1000

    Implementation

---
+++ README.md
@@ -0,0 +1,36 @@
+# Photo Repack
+
+A simple tool which processes images into a more compact form, with (hopefully) zero data loss. Unsupported files will be copied.
+
+## Roadmap
+
+### Xplat Paths
+
+Rework paths to have a high level of cross environment compatibility on public interfaces and universal path formats internally (using `file://` approach).
+
+### Video Support
+
+Effectively encoding a video file for size is a taxing process, particularly for a mobile device which increasingly are the source of such content. Hardware level video processing improvements help, but will always run into a wall somewhere (plus this does nothing for existing content).
+
+Support for repacking video files might provide significant disk space savings in some circumstances, provided the right formats are supported and algoirthms in place.
+
+This is not an easy feature to implement right, and will be taxing on hardware. Early bail-out functionality is a must.
+
+### Alternate Compression Libraries
+
+There are a lot of compression libraries in the wild, particularly beyond the .NET ecosystem. Using these provides an opportunity for greater space savings.
+
+WASM is likely the most portable approach, however FFI generation is an issue. Langauges like C and Rust tend to generate code for the interop (usuallyy JS with optional TS annotations) which makes consumption from different languages more difficult (though not impossible provided WASM can be used).
+
+* Squoosh
+  Has C source which can compile into WASM.
+* Photon
+  Rust library with WASM target.
+
+### File Format Changing
+
+Some file formats (especially newer ones) can compress better, on example is JXL which is intended to supersede JPEG.
+
+### Compress Known Digital Negatives
+
+Digital negatives are most valuable for post processing, and are often unused. Support for compressing these using a general purpose archive may provide significant savings. It should be possible to repack if reprocessed (to enable existing photo collections to further enhanced as improvements are made to the tool).
--- program.cs
+++ program.cs
@@ -1,12 +1,83 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using ImageMagick;
 
 namespace PhotoRepack
 {
     class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// </summary>
+        /// <param name="inputDir">The input directory images will be read from.</param>
+        /// <param name="outputDir">The output directory images will be written to.</param>
+        static void Main(string? inputDir, string? outputDir)
         {
-            Console.WriteLine("Hello World!");
+            if (string.IsNullOrEmpty(inputDir)) {
+                Console.Error.WriteLine("--input-dir is required");
+                return;
+            }
+            if (string.IsNullOrEmpty(outputDir)) {
+                Console.Error.WriteLine("--output-dir is required");
+                return;
+            }
+
+            // Normalise path
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
+                // Patch user path
+                string userProfile = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "/";
+                inputDir = inputDir.Replace("~/", userProfile);
+                inputDir = inputDir.Replace("~\\", userProfile);
+                outputDir = outputDir.Replace("~/", userProfile);
+                outputDir = outputDir.Replace("~\\", userProfile);
+            }
+            inputDir = Path.GetFullPath(inputDir);
+            outputDir = Path.GetFullPath(outputDir);
+
+            // Fuck-up guard, input is output
+            if (inputDir == outputDir) {
+                Console.Error.WriteLine("--input-dir is same as --output-dir, overwriting not allowed");
+                return;
+            }
+
+            // Fuck-up guard, output in input
+            if (outputDir.Contains(inputDir)) {
+                Console.Error.WriteLine("--output-dir is inside --input-dir, aborting to prevent recursion");
+                return;
+            }
+
+            // Fuck-up guard, input in output
+            if (inputDir.Contains(outputDir)) {
+                Console.Error.WriteLine("--input-dir is inside --output-dir, overwriting not allowed");
+                return;
+            }
+
+            // Lets get to work
+
+            // Find files in input
+            var files = Directory.GetFiles(inputDir, "**", new EnumerationOptions() {
+                RecurseSubdirectories = true,
+            });
+
+            var optimizer = new ImageOptimizer();
+            optimizer.OptimalCompression = true;
+            foreach (var file in files)
+            {
+                Console.Out.WriteLine($"Processing {file}");
+                try {
+                    var dest = file.Replace(inputDir, outputDir);
+                    var destParent = Directory.GetParent(dest);
+                    if (destParent is not null) {
+                        destParent.Create();
+                    }
+                    File.Copy(file, dest);
+                    optimizer.LosslessCompress(dest);
+                }
+                catch (Exception e) {
+                    Console.Error.WriteLine("An error occurred");
+                    Console.Error.WriteLine(e);
+                }
+            }
         }
     }
 }
